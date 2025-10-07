94fc71e516abcb594c6450b82fcc9592de46838d
mala91027 <malakumaripandey841419@gmail.com>
2025-10-07 21:11:55 +0530

    Add files via upload

---
+++ insert data into sql.sql
@@ -0,0 +1,10 @@
+select * from employee;
+
+
+insert into employee(name,position,department,hire_date,salary)
+    ('mala', 'web development', 'data science', '2002-05-07',65000),
+    ('priya', 'web development', 'data science', '2002-05-07',65000),
+    ('malu', 'web development', 'data science', '2002-05-07',65000),
+	
+    ('dala', 'web development', 'data science', '2002-05-07',65000),
+    ('sona', 'web development', 'data science', '2002-05-07',65000),
